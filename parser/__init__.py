8e89d32f0eb52e70ba07608b37c2c7f39b29bda6
Anisimov Gleb <119005542+ice-rider@users.noreply.github.com>
2025-02-16 18:37:08 +0400

    Update __init__.py

--- parser/__init__.py
+++ parser/__init__.py
@@ -1,9 +1,11 @@
 from .types_ import Int, String, Float, Bool
 from .Scheme import Scheme, make_scheme
+from .Exception import ValidationError
 
 
 __all__ = [
     'Scheme',
+    'ValidationError'
     'make_scheme',
     'Int',
     'String',
